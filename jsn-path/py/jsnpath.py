b0baecd45b0f3c3357e311cebd2983d611a4ec46
Bill Blondeau <whblondeau@gmail.com>
2017-03-28 08:49:30 -0500

    initial work on python impl

--- jsn-path/py/jsnpath.py
+++ jsn-path/py/jsnpath.py
@@ -14,6 +14,11 @@ def match_tupl(pattern, path):
 
 # --------------------------------------- FUNCTIONS
 
+def step_empty(stepcontent):
+    return step == ''
+
+
+
 def match_tupl(pattern, path):
     '''both arguments are tuples'''
     if len(path) < len(pattern):
@@ -26,14 +31,15 @@ def match_tupl(pattern, path):
 
 
 def enumerate_leafpaths(current_node, current_pathstring='', current_steplist=[]):
-    '''This function accepts a parsed JSON object and 
+    '''INTERNAL: This function accepts a parsed JSON object and 
     returns a generator that, when iterated, provides 
     3-tuples of all leaf paths in the instance. 
     The 3-tuple is of the form (pathstring, steplist, leafnode).
     Note that pathstring and steplist are logically equivalent. This method
     provides both because they afford different conveniences for
     the various JSNPath evaluation techniques that will operate on these
-    leafpaths.'''
+    leafpaths.
+    Note that this is '''
 
     # children to iterate through?
     if isinstance(current_node, list):
@@ -101,47 +107,121 @@ def build_analysis_dicts(jsonobj):
 
 
 
-# INTERNAL
-# def recurse_json(id, value, path):
-#     '''JSON nodes can be container types (`dict`, `list`) or value types
-#     (`string`, `boolean`, `number` the various undefined types.)'''
-#     if isinstance(value, list):
-#         return [recurse_json(child) for child in 
-
-
-# def capitalize(x):
-#    if isinstance(x, list):
-#      return [capitalize(v) for v in x]
-#    elif isinstance(x, dict):
-#      return {k[0].upper() + k[1:]: capitalize(v) for k, v in x.items()}
-#    else:
-# return x
-
-# def enumerate(self, path=None):
-#     """Iterate through the PelicanJson object yielding 1) the full path to
-#     each value and 2) the value itself at that path.
-#     """
-#     if path is None:
-#         path = []
-#     for k, v in self.store.items():
-#         current_path = path[:]
-#         current_path.append(k)
-
-#         if isinstance(v, PelicanJson):
-#             yield from v.enumerate(path=current_path)
-#         elif isinstance(v, list):
-#             for idx, list_item in enumerate(v):
-#                 list_path = current_path[:]
-#                 list_path.append(idx)
-#                 if isinstance(list_item, PelicanJson):
-#                     yield from list_item.enumerate(path=list_path)
-#                 else:
-#                     yield list_path, list_item
-#         else:
-#             yield current_path, v
-
 # --------------------------------------- CONSTANTS
+
+# BUILDING BLOCKS: regexes or regex fragments that are useful
+# in their own right but mostly serve to populate `jsnpath_regexes`
+
+wildcard_index_pattern = '(' + '\[\]' + ')'
+
+num_pattern = '-?[0-9]+'
+
+single_index_pattern = '(' + num_pattern + ')'
+
+slice_startwith_pattern = '((' + num_pattern + ')[:])'
+slice_stopbefore_pattern = '([:](' + num_pattern + '))'
+slice_startwith_stopbefore_pattern = '((' + num_pattern + ')[:](' + num_pattern + '))'
+
+slice_pattern =  slice_startwith_pattern
+slice_pattern += '|'
+slice_pattern += slice_stopbefore_pattern
+slice_pattern += '|'
+slice_pattern += slice_startwith_stopbefore_pattern
+
+index_pattern =  wildcard_index_pattern
+index_pattern += '|'
+index_pattern += '(' + '\[(' + single_index_pattern
+index_pattern += '|'
+index_pattern += slice_pattern + ')\]' + ')'
+
+# more API-oriented exposure of specific regexes
+jsnpath_regex = {
+    'name_literal_content': '([A-Za-z0-9]{1,2})|([A-Za-z0-9][A-Za-z0-9_]*[A-Za-z0-9])',
+
+    'name_globbed_content': '([A-Za-z0-9*+]{1,2})|([A-Za-z0-9][A-Za-z0-9*+_]*[A-Za-z0-9*+])',
+
+    'index_literal_content': single_index_pattern,
+    'index_slicing_content': slice_pattern,
+}
+
+# The distinguished names for types of step in
+# a JSNPath expression
+step_types = {
+    'name_literal':
+    {
+        'description':  '''The step has quoted content, and contains
+                            nothing but alphanumeric characters
+                            (upper or lower case) and non-leading,
+                            non-trailing underscores.''',
+
+        'content_regex':    '(("' + jsnpath_regex['name_literal_content'] + '")'
+                            + '|'
+                            + "('" + jsnpath_regex['name_literal_content'] + "'))"
+    },
+
+    'name_globbed':
+    {
+        'description':  '''The node name is expressed with at least one `*`
+                            glob symbol, which is a subsitition notiation
+                            for 0-n characters.''',
+
+        'content_regex':    '(("' + jsnpath_regex['name_globbed_content'] + '")'
+                            + '|'
+                            + "('" + jsnpath_regex['name_globbed_content'] + "'))"
+    },
+
+    'index_literal':
+    {
+        'description':  '''The step has UNQUOTED content, and contains
+                            nothing but numbers with optional leading 
+                            minus sign `-`.
+                            (Note that negative indexing is done from the
+                            end of the array, and is 1-based counting: 
+                            `array[-1]` is the last item in the array.''',
+
+        'content-regex':    jsnpath_regex['index_literal_content']
+    },
+
+    'index_slice': 
+    {
+
+    },
+
+    'index_wildcard':
+    {  
+        'description':  '''The step represents ANY index (numeric) literal, and is
+                            therefore a selector for ALL children of the 
+                            parent array. The step is an empty pair of
+                            brackets, `[]`, and the content is therefore
+                            the empty string. (Note that the content_regex
+                            does not work when using multiline mode; but
+                            this is a degenerate case anyway. Step content
+                            can be tested against the empty string in many
+                            efficient ways wihout recourse to regex!''',
+
+        'content_regex':    '^$',
+
+        'content_eval': step_empty,
+    },
+
+    'index':
+    {
+
+    }
+
+}
+
+
+
+
+# This dictionary maps non-literal steps in a JSNPath
+# expression to the corresponding
+# stepwise matching logic.
+# Each key is a generic step type
+match_dict = {
+    
+}
 
 # --------------------------------------- EXECUTE
 
-
+print('NO GROSS SYNTAX ERROS.')
